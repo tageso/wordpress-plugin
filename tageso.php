47a7c5baae24cb2b36e5a30ccf94da4ce1d8e68f
Sören <git@kekskurse.de>
2018-09-25 23:53:50 +0200

    Update tageso.php

--- tageso.php
+++ tageso.php
@@ -7,14 +7,13 @@
 * Author URI: https://www.tageso.de
 */
 
-// Example 1 : WP Shortcode to display form on any page or post.
+
 function tageso_protocoll_list($args){
     $res = file_get_contents("https://api.tageso.de/agenda/".$args["agenda"]."/protocol");
     $data = json_decode($res);
     foreach($data->data as $p) {
         echo '<li><a href="https://app.tageso.de/#/agenda/'.$args["agenda"].'/protocol/'.$p->_id.'">'.$p->date."</a></li>";
     }
-    #var_dump($res);
 }
 add_shortcode('tageso_protocoll_list', 'tageso_protocoll_list');
 ?>
